106dbf6a23754b4fe82b1b1d5d984a27ebd73138
SGSANJAY044 <sgsanjay044@gmail.com>
2024-06-25 16:31:37 +0530

    feat(Product): This commit with create a page with product details

---
+++ src/components/Product/Nav.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import logo from '../../assets/surveysparrow_logo.jpeg'
+import { Flex, Avatar, Box } from '@sparrowengg/twigs-react'
+
+function Nav() {
+    return (
+        <Flex css={{
+            height: 80,
+            margin: 10,
+            padding: 20,
+            borderRadius: 20,
+            background: '$primary300'
+        }} alignItems='center' justifyContent='space-between'>
+            <Flex alignItems='center' gap={20}>
+                <Avatar src={logo} name='sparrow' size='2xl' />
+                <Box css={{
+                    fontSize: '$2xl',
+                    color: '$white900',
+                    fontFamily: 'sans-serif',
+                    letterSpacing: 1,
+                    fontWeight: '$9'
+                }} >SPARROWMART</Box>
+            </Flex>
+            <Flex>
+            </Flex>
+        </Flex>
+    )
+}
+
+export default Nav
--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,11 +1,85 @@
-import React from 'react'
+import API from 'api';
+import Nav from 'components/Product/Nav';
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import { Tabs, TabsList, TabsTrigger, TabsContent, Box, Button, Flex } from "@sparrowengg/twigs-react";
+import { FaBagShopping, FaStar } from 'react-icons/fa6';
+import { FaShoppingCart } from 'react-icons/fa';
+import ProductCard from 'components/Product/ProductCard';
 
 function Product() {
-    const location = useLocation()
-    console.log(location.pathname.split('/')[2]);
+    const location = useLocation();
+    const [productData, setProductData] = useState(null);
+    const [productReviews, setProductReviews] = useState(null);
+    const productId = location.pathname.split('/')[2];
+
+    const getProductData = async () => {
+        try {
+            const data = await API.get(`/product/${productId}`)
+            setProductData(data.data)
+        }
+        catch (err) {
+            console.log("Cannot Fetch Product Data", err)
+        }
+    }
+
+    const getProductReviews = async () => {
+        try {
+            const data = await API.get(`/reviews/${productId}`)
+            setProductReviews(data.data)
+        }
+        catch (err) {
+            console.log("Cannot Fetch Product Data", err)
+        }
+    }
+
+    console.log(productData);
+    console.log(productReviews);
+    useEffect(() => {
+        getProductData()
+        getProductReviews()
+    }, [])
+
+
     return (
-        <>Single Product Page</>
+        <>
+            <Nav />
+            {
+                productData ?
+                    <Flex css={{
+                        height: '80vh',
+                        padding: 20
+                    }} alignItems='center' justifyContent='center'>
+                        <Flex css={{ height: '100%', width: '100%' }} alignItems='center' justifyContent='center'>
+                            <img src={productData.image} alt="" style={{ height: '100%' }} />
+                        </Flex>
+                        <Flex
+                            css={{
+                                height: '100%',
+                                width: '100%',
+                                boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;',
+                                borderRadius: 20,
+                                padding: 20,
+                                paddingBottom: 40
+                            }}>
+                            <Tabs defaultValue="tab1">
+                                <TabsList aria-label="tabs example">
+                                    <TabsTrigger value="tab1"> About </TabsTrigger>
+                                    <TabsTrigger value="tab2"> Reviews </TabsTrigger>
+                                </TabsList>
+                                <TabsContent value="tab1">
+                                    <ProductCard productData={productData} />
+                                </TabsContent>
+                                <TabsContent value="tab2">
+                                    Tab 2
+                                </TabsContent>
+                            </Tabs>
+                        </Flex>
+                    </Flex>
+                    :
+                    <>Data is Fetching</>
+            }
+        </>
     )
 }
 
